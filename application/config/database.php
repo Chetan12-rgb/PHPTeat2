f7464ff11ee230001e97e2ad6ac313cb754a5977
Chetan12-rgb <142248955+Chetan12-rgb@users.noreply.github.com>
2024-08-30 13:00:07 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '172.17.0.3',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'strong_pass',
 	'database' => 'lof',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
